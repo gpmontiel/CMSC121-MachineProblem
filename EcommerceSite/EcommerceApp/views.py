c65727eb5ff72a97a6c8ba6f93d7a41292db7b04
Nira-nix <nccifra@up.edu.ph>
2025-04-16 18:49:07 +0800

    base htmls,views and urls

--- EcommerceSite/EcommerceApp/views.py
+++ EcommerceSite/EcommerceApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from . models import *
+# from . forms import CreateUserForm
 
 # Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+def cart(request):
+    return render(request, "cart.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
+    
