84ba9c5435434ecfa20afcd266bffdb1dc8f02fe
Nira-nix <nccifra@up.edu.ph>
2025-04-16 20:32:45 +0800

    Added the backend logic for the registration and login pages
    Implemented the login and registration page, plus added some templates to test the code. Added a home_user to be able to differentiate registered and not registered users

--- EcommerceSite/EcommerceApp/models.py
+++ EcommerceSite/EcommerceApp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+    
--- EcommerceSite/EcommerceApp/urls.py
+++ EcommerceSite/EcommerceApp/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
-    path("cart/", views.cart, name="cart")
+    path("login/", views.login_user, name="login"),
+    path("register/", views.register_user, name="register"),
+    path("cart/", views.cart, name="cart"),
+    path("logout/", views.logout_user, name="logout"),
+    path("user/<str:username>/", views.home_user, name="home_user")
 ]
